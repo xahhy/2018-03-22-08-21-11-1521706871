8cf5dd9daae87f79033dfcc0fce90b29fdcdd4f4
hhy <1014596312@qq.com>
2018-04-06 13:09:25 +0800

    [6]

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,26 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Integer klass = null;
+
+    Teacher(String name, Integer age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, Integer age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (klass == null){
+            return String.format("%s I am a Teacher. I teach No Class.", super.introduce());
+        }
+        return String.format("%s I am a Teacher. I teach Class %s.", super.introduce(), klass);
+    }
 }
