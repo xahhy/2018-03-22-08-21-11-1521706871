51c4f61b0381eccd86ebc13219022a8a09868c80
hhy <1014596312@qq.com>
2018-04-07 01:39:40 +0800

    [9]

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,46 @@
 package practice09;
 
 public class Klass {
+    private Integer number;
+    private String displayName;
+    private Student leader;
+
+    public Klass(Integer number) {
+        this.number = number;
+        this.displayName = "Class " + number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void assignLeader(Student student) {
+        if(this.equals(student.getKlass())) {
+            this.leader = student;
+        }else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student student) {
+        student.setKlass(this);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) return true;
+        if(obj == null) return false; //能调用这个方法，this肯定不为null，所以不判断this
+        if(this.getClass() != obj.getClass()) return false; //如果不死同一个类，则必然false
+        Klass that = (Klass) obj;
+        return that.getNumber().equals(this.number);
+
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,38 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass = null;
+
+    Teacher(Integer id, String name, Integer age) {
+        super(id, name, age);
+    }
+
+    public Teacher(Integer id, String name, Integer age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String basicIntroduce(){
+        return String.format("%s I am a Teacher.", super.introduce());
+    }
+
+    @Override
+    public String introduce() {
+        if (klass == null){
+            return String.format("%s I teach No Class.", basicIntroduce());
+        }
+        return String.format("%s I teach %s.", basicIntroduce(), klass.getDisplayName());
+    }
+
+    public String introduceWith(Student student) {
+        if (!student.getKlass().getNumber().equals(this.klass.getNumber())) {
+            return String.format("%s I don't teach %s.", this.basicIntroduce(), student.getName());
+        }else {
+            return String.format("%s I teach %s.", this.basicIntroduce(), student.getName());
+        }
+    }
 }
