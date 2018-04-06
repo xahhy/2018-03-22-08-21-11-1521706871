ae6fa290b1ad8b9bce294b025575670c694c92d2
hhy <1014596312@qq.com>
2018-04-06 12:54:50 +0800

    [2]

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,24 @@
 package practice02;
 
 public class Person {
+    private final String name;
+    private final Integer age;
+
+    Person(String name, Integer age) {
+
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %s years old.", name, age);
+    }
 }
