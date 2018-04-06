23e263b474a2cec7a2cc080d6b2c020b29be31ca
hhy <1014596312@qq.com>
2018-04-06 13:01:20 +0800

    [4]

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,19 @@
 package practice04;
 
-public class Student {
+public class Student extends Person {
+    private final Integer klass;
+
+    Student(String name, Integer age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("%s I am a Student. I am at Class %s.", super.introduce(), klass);
+    }
 }
